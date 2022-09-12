38d878e20266ab44122cc9084e4eb88cb40f315c
IiCR3ATORiI <ali2010ali06@gmail.com>
2022-09-12 20:27:04 +0500

    add: add javascript

--- script/main.js
+++ script/main.js
@@ -1,5 +1,16 @@
+"use strict";
 let mainBtn = document.querySelector("button");
 console.log(mainBtn);
 mainBtn.addEventListener("click", function () {
 	alert("Нет это Оптимус Прайм");
 });
+let a = 5 + +5;
+console.log(a);
+let myname = "ALI";
+let mysurname = " BEKBOLAT";
+console.log(myname + mysurname);
+console.log(typeof null);
+function plus(a, b) {
+	return a + b;
+}
+console.log(plus(10, 7));
